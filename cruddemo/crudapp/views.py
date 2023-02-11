d122b2e5a389f7930a7c47fef9c63eec63d861c4
sathish <sathishjai2001@gmail.com>
2023-02-11 11:11:02 +0530

    additional of student list view another page view,html,urls edited

--- cruddemo/crudapp/views.py
+++ cruddemo/crudapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from crudapp.models import student
 # Create your views here.
+
+def retrieve_view(request):
+#    student=
+    return render(request,'crudapp/index.html',{'student':student.objects.all()})
